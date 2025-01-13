51f9b9402fda865d746414e8cd18e66143c94af8
Mahmoud K.Abu Sultan <82243460+MahmoudKSultan@users.noreply.github.com>
2025-01-13 06:36:18 +0200

    Update index.tsx

--- src/features/auth/login/index.tsx
+++ src/features/auth/login/index.tsx
@@ -49,23 +49,7 @@ export function LoginForm({
         <Button type="submit" className="w-full" asChild>
           <Link href="/wizard">Sign in</Link>
         </Button>
-        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-          <span className="relative z-10 bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-        <Button variant="outline" className="w-full" asChild>
-          <Link href="/wizard">
-            {" "}
-            Sign in with Linkedin
-            <Image
-              src="/assets/images/logos/linkedin.png"
-              alt="Linkedin logo"
-              width={16}
-              height={16}
-            />
-          </Link>
-        </Button>
+     
       </div>
       <div className="text-center text-sm">
         Don&apos;t have an account?{" "}

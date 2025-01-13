a505f43a110ffa51fce8399827d286dd8a3f491d
MahmoudKSultan <mahmoodkns@hotmail.com>
2025-01-13 07:25:31 +0200

    logos

--- src/app/(auth)/wizard/page.tsx
+++ src/app/(auth)/wizard/page.tsx
@@ -5,42 +5,132 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const banks = [
+const dentalSoftwareCompanies = [
   {
-    name: "Discover",
-    website: "https://www.discover.com",
-    image: "/discover.png", // Replace with the actual image path
+    name: "Weave",
+    website: "https://www.getweave.com",
+    image: "/weave.avif",
   },
   {
-    name: "Fidelity",
-    website: "https://www.fidelity.com",
-    image: "/fed.jpg", // Replace with the actual image path
+    name: "RevenueWell",
+    website: "https://www.revenuewell.com",
+    image: "/rw.avif",
   },
   {
-    name: "Wells Fargo",
-    website: "https://www.wellsfargo.com",
-    image: "/wells-fargo.png", // Replace with the actual image path
+    name: "Thryv",
+    website: "https://www.thryv.com",
+    image: "/thryy.avif",
   },
   {
-    name: "Huntington Bank",
-    website: "https://www.huntington.com",
-    image: "/huntington.png", // Replace with the actual image path
+    name: "Solutionreach",
+    website: "https://www.solutionreach.com",
+    image: "/sol.avif",
   },
   {
-    name: "Ally",
-    website: "https://www.ally.com",
-    image: "/ally.jpg", // Replace with the actual image path
+    name: "Adit",
+    website: "https://www.adit.com",
+    image: "/adit.avif",
   },
   {
-    name: "USAA",
-    website: "https://www.usaa.com",
-    image: "/usaa.png", // Replace with the actual image path
+    name: "Doctible",
+    website: "https://www.doctible.com",
+    image: "/docitble.avif",
   },
   {
-    name: "American Express",
-    website: "https://www.amex.com",
-    image: "/ae.jpg", // Replace with the actual image path
+    name: "Acuity Scheduling",
+    website: "https://www.capterra.com/p/191978/Acuity-Scheduling/",
+    image: "/aucity.avif",
   },
+  {
+    name: "Dentrix Ascend",
+    website: "https://www.dentrixascend.com",
+    image: "/d-ascend.avif",
+  },
+  {
+    name: "Eaglesoft",
+    website: "https://www.eaglesoft.com",
+    image: "/eagle.avif",
+  },
+  {
+    name: "Curve Dental",
+    website: "https://www.curvedental.com",
+    image: "/curve.avif",
+  },
+  {
+    name: "Practice-Web",
+    website: "https://www.practice-web.com",
+    image: "/pw.avif",
+  },
+  {
+    name: "ACE Dental",
+    website: "https://www.capterra.com/p/10010533/ACE-Dental/",
+    image: "/ace.avif",
+  },
+  {
+    name: "Denticon",
+    website: "https://www.planetdds.com/denticon",
+    image: "/dentiction.avif",
+  },
+  {
+    name: "ABELDent",
+    website: "https://www.abeldent.com",
+    image: "/ab.avif",
+  },
+  {
+    name: "Open Dental",
+    website: "https://www.opendental.com",
+    image: "/od.avif",
+  },
+  {
+    name: "DentiMax",
+    website: "https://www.dentimax.com",
+    image: "/dm.avif",
+  },
+  // {
+  //   name: "Carestream Dental",
+  //   website: "https://www.carestreamdental.com",
+  //   image: "/carestreamdental",
+  // },
+  // {
+  //   name: "Easy Dental",
+  //   website: "https://www.easydental.com",
+  //   image: "/easydental",
+  // },
+  {
+    name: "Dentrix",
+    website: "https://www.dentrix.com",
+    image: "/dentrix.avif",
+  },
+  // {
+  //   name: "Mogo Cloud Dental Software",
+  //   website: "https://www.mogo.com",
+  //   image: "/mogoclouddentalsoftware",
+  // },
+  // {
+  //   name: "iDentalSoft",
+  //   website: "https://www.identalsoft.com",
+  //   image: "/identalsoft",
+  // },
+  // {
+  //   name: "Tab32",
+  //   website: "https://www.tab32.com",
+  //   image: "/tab32",
+  // },
+  // {
+  //   name: "Maxident",
+  //   website: "https://www.maxidentsoftware.com",
+  //   image: "/maxident",
+  // },
+  // {
+  //   name: "Dovetail",
+  //   website: "https://www.dovetaildentalsoftware.com",
+  //   image: "/dovetail",
+  // },
+  // {
+  //   name: "Umbie DentalCare",
+  //   website: "https://www.umbiedentalcare.com",
+  //   image: "/umbiedentalcare",
+  // },
 ];
 
 function Wizrd() {
@@ -48,7 +138,7 @@ function Wizrd() {
     <>
       <div className="text-center mb-10">
         <p className="text-2xl text-center mb-5 font-semibold">
-          Connect Your Calender
+          Connect Your PMS
         </p>
         <p className="text-[#929799] text-xl">
           What appointment booking system do you currently use?
@@ -67,7 +157,7 @@ function Wizrd() {
           />
         </div>
         <div className="flex flex-wrap">
-          {banks.map((bank) => (
+          {dentalSoftwareCompanies.map((bank) => (
             <Link
               href={bank.website}
               className="p-1 w-1/2 md:w-1/3 lg:w-1/4"
@@ -103,9 +193,9 @@ function Wizrd() {
         </p>
 
         <div className="pt-5 border-t">
-        <Button type="submit" className="w-full rounded-full h-12" asChild>
-          <Link href="/calender">Continue</Link>
-        </Button>
+          <Button type="submit" className="w-full rounded-full h-12" asChild>
+            <Link href="/calender">Continue</Link>
+          </Button>
         </div>
       </div>
     </>

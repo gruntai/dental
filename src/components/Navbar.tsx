3755d5f82d29bb54a6bf68973b17f0e1e824480c
MahmoudKSultan <mahmoodkns@hotmail.com>
2025-02-12 00:24:45 +0200

    navbar add icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,32 +12,7 @@ function Navbar() {
   const [checked, setChecked] = React.useState(true);
   return (
     <nav className="bg-white fixed left-0 top-0 z-50 w-full flex items-center justify-between px-5 lg:px-10 py-3 pt-5 border-b border-black">
-      <div className="flex gap-2 lg:gap-20 items-center">
-        <Sheet>
-          <SheetTrigger className="block lg:hidden ">
-            <MenuIcon size={24} />
-          </SheetTrigger>
-          <SheetContent side={"left"}>
-            <SheetHeader className="h-full">
-              <div className="flex flex-col h-full">
-                <div className="flex-1 h-full">
-                  <NavigationList />
-                </div>
-                <Link
-                  href={"/account-settings"}
-                  className="flex items-center gap-2 cursor-pointer"
-                >
-                  <div className="w-11 h-11  bg-[#E8D7FE] rounded-full flex items-center justify-center text-white">
-                    SL
-                  </div>
-                  <span className="text-[#737278] text-sm font-semibold">
-                    Account Settings
-                  </span>
-                </Link>
-              </div>
-            </SheetHeader>
-          </SheetContent>
-        </Sheet>
+      <div className="flex gap-20 items-center">
         <Image
           src="/assets/images/logos/grunt_logo.png"
           alt="grunt logo"
@@ -68,10 +43,45 @@ function Navbar() {
           </div>
 
           <Mail
-            className="w-[18px] h-[18px] ml-3 cursor-pointer hidden sm:block"
+            className="w-[18px] h-[18px] ml-3 cursor-pointer hidden lg:block"
             strokeWidth={2}
           />
-          <Bell className="w-[18px] h-[18px] cursor-pointer hidden sm:block" strokeWidth={2} />
+          <Bell
+            className="w-[18px] h-[18px] cursor-pointer hidden lg:block"
+            strokeWidth={2}
+          />
+          <Sheet>
+            <SheetTrigger className="block lg:hidden ">
+              <MenuIcon size={24} />
+            </SheetTrigger>
+            <SheetContent side={"left"}>
+              <SheetHeader className="h-full">
+                <Image
+                  src="/assets/images/logos/grunt_logo.png"
+                  alt="grunt logo"
+                  width={120}
+                  height={30}
+                  className="mb-10"
+                />
+                <div className="flex flex-col h-full">
+                  <div className="flex-1 h-full">
+                    <NavigationList />
+                  </div>
+                  <Link
+                    href={"/account-settings"}
+                    className="flex items-center gap-2 cursor-pointer"
+                  >
+                    <div className="w-11 h-11  bg-[#E8D7FE] rounded-full flex items-center justify-center text-white">
+                      SL
+                    </div>
+                    <span className="text-[#737278] text-sm font-semibold">
+                      Account Settings
+                    </span>
+                  </Link>
+                </div>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
     </nav>

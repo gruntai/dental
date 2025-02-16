6b3cd7b8c5bf0f89b4ae531758aa5ff2d0344c86
MahmoudKSultan <mahmoodkns@hotmail.com>
2025-02-16 21:18:55 +0200

    m

--- src/features/patient-waitlist/Table.tsx
+++ src/features/patient-waitlist/Table.tsx
@@ -253,31 +253,35 @@ export function MYTable() {
   return (
     <div className="w-full">
       <div className="flex items-center gap-2 mb-5">
-        <Button
-          className="flex items-center border-[#72A094]"
-          variant="outline"
-        >
-          <Filter />
-          Filter
-        </Button>
-        <Button
-          className="flex items-center border-[#72A094]"
-          variant="outline"
-        >
-          <Image
-            src="/assets/images/icons/settings.png"
-            width="13"
-            alt="settings icon"
-            height="14"
-          />
-          <span>Settings</span>
-          <Image
-            src="/assets/images/icons/downArrow.png"
-            width="7"
-            alt="arrow down"
-            height="5"
-          />
-        </Button>
+        <SelectComp
+          data={appointmentTypes}
+          label={
+            <div className="flex items-center gap-2">
+              <Filter size={14} />
+              Filter
+            </div>
+          }
+        />
+        <SelectComp
+          data={settings}
+          label={
+            <div className="flex items-center gap-2">
+              <Image
+                src="/assets/images/icons/settings.png"
+                width="13"
+                alt="settings icon"
+                height="14"
+              />{" "}
+              <span>Settings</span>
+              <Image
+                src="/assets/images/icons/downArrow.png"
+                width="7"
+                alt="arrow down"
+                height="5"
+              />
+            </div>
+          }
+        />
       </div>
       <div className="rounded-md border">
         <Table>
@@ -332,3 +336,70 @@ export function MYTable() {
     </div>
   );
 }
+
+import {
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Select,
+} from "@/components/ui/select";
+import React from "react";
+
+const softwareList = [
+  { id: 1, label: "Dentrix Ascend", value: "dentrix-ascend" },
+  { id: 2, label: "Eaglesoft Cloud", value: "eaglesoft-cloud" },
+  { id: 3, label: "Open Dental Cloud", value: "open-dental-cloud" },
+  { id: 4, label: "CareStack", value: "carestack" },
+  { id: 5, label: "Tab32", value: "tab32" },
+  { id: 6, label: "DentiMax Cloud", value: "dentimax-cloud" },
+  { id: 7, label: "Curve Dental", value: "curve-dental" },
+  { id: 8, label: "Dentrix", value: "dentrix" },
+  { id: 9, label: "Eaglesoft", value: "eaglesoft" },
+  { id: 10, label: "Open Dental", value: "open-dental" },
+  { id: 11, label: "DentiMax", value: "dentimax" },
+  { id: 12, label: "SoftDent", value: "softdent" },
+  { id: 13, label: "PracticeWorks", value: "practiceworks" },
+  { id: 14, label: "OrthoTrac", value: "orthotrac" },
+  { id: 15, label: "ABELDent", value: "abeldent" },
+  { id: 16, label: "MOGO Cloud", value: "mogo-cloud" },
+  { id: 17, label: "axiUm", value: "axium" },
+  { id: 18, label: "Fuse", value: "fuse" },
+  { id: 19, label: "MacPractice DDS", value: "macpractice-dds" },
+  { id: 20, label: "Dovetail", value: "dovetail" },
+];
+const appointmentTypes = [
+  { label: "Emergency", value: "emergency" },
+  { label: "Routine", value: "routine" },
+  { label: "Urgent", value: "urgent" },
+  { label: "Elective", value: "elective" },
+];
+const settings = [
+  { label: "Refresh", value: "Refresh" },
+  { label: "Stop Running", value: "StopRunning" },
+];
+export function SelectComp({ data, label }: { label: Element; data: any[] }) {
+  const [software, setSoftware] = React.useState(data[0].value);
+  return (
+    <Select name="Country" value={software} onValueChange={setSoftware}>
+      <SelectTrigger
+        hideChevron={true}
+        className="w-fit rounded-[4px]  text-[#7DA295]  border-[#72A094] text-sm font-normal"
+      >
+        {label}
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup className="text-black/60 text-sm">
+          {data.map((item) => (
+            <SelectItem key={item.value} value={item.value}>
+              {item.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+}
+
+export default SelectComp;
